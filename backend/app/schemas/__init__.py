08d9338cd220247d43bd33459c750a0ca14906ef
Mori Yuta <yuta.m.12.1214@gmail.com>
2025-06-06 10:19:52 +0900

    スキーマのモジュール化

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,10 @@
+from app.schemas.user import UserCreate, UserRead
+from app.schemas.expense import ExpenseCreate, ExpenseRead
+
+
+__all__ = [
+    "UserCreate",
+    "UserRead",
+    "ExpenseCreate",
+    "ExpenseRead",
+]

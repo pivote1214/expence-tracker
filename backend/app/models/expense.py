e83cc87e8eccd4f9b8966f2db83973659fc1699a
Mori Yuta <yuta.m.12.1214@gmail.com>
2025-06-22 22:26:06 +0900

    add came case logic

--- backend/app/models/expense.py
+++ backend/app/models/expense.py
@@ -18,7 +18,7 @@ class Expense(BaseModel):
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     detail: Mapped[str] = mapped_column(String(500), nullable=True)
     amount: Mapped[int] = mapped_column(Integer, nullable=False)
-    payment_date: Mapped[Date] = mapped_column(Date, nullable=False)
+    payment_date: Mapped[date] = mapped_column(Date, nullable=False)
     ratio: Mapped[float] = mapped_column(Float, nullable=False)
 
     payer_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
@@ -28,7 +28,7 @@ class Expense(BaseModel):
         self,
         title: str,
         detail: str,
-        amount: str,
+        amount: int,
         payment_date: date,
         ratio: float,
         payer_id: int,
--- backend/app/schemas/base.py
+++ backend/app/schemas/base.py
@@ -2,4 +2,10 @@ from pydantic import BaseModel, ConfigDict
 
 
 class BaseSchema(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(
+        from_attributes=True,
+        populate_by_name=True,
+        alias_generator=lambda x: "".join(
+            word.capitalize() if i > 0 else word for i, word in enumerate(x.split("_"))
+        ),
+    )

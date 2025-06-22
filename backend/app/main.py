12f25c4c894d9abd11e966d218875e8386126689
Mori Yuta <yuta.m.12.1214@gmail.com>
2025-06-22 22:25:51 +0900

    add CORS settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,20 @@
+from app.api import expense, user
 from fastapi import FastAPI
-
-from app.api import user, expense
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user.router, prefix="/api", tags=["users"])
 app.include_router(expense.router, prefix="/api", tags=["expense"])
+
 
 @app.get("/")
 async def root():
